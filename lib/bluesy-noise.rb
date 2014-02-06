27e2a97c4567487d6300c2a6b02f64e39b2d6d8b
Paul Prince <paul@littlebluetech.com>
2014-02-06 08:37:36 -0500

    wip

--- lib/bluesy-noise.rb
+++ lib/bluesy-noise.rb
@@ -1,6 +1,8 @@
 require 'compass'
-require "chunky_png"
-require "base64"
+require 'chunky_png'
+require 'base64'
+require 'rubystats/normal'
+
 require File.join(File.dirname(__FILE__), 'bluesy-noise', 'sass_extensions')
 
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
